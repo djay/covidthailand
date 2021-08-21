ffbc5e2da1fe74cd28874ef71b2f5669df3d78bd
Dylan Jay <software@pretaweb.com>
2021-08-21 22:45:42 +0700

    increase timeout for dashbaord to 30s

--- utils_scraping.py
+++ utils_scraping.py
@@ -635,7 +635,7 @@ def workbooks(url, skip=None, dates=[], **selects):
         print("MOPH Dashboard", f"Error: Empty Worksheet url {url}")
         return
 
-    fix_timeouts(ts.session, timeout=15)
+    fix_timeouts(ts.session, timeout=30)
     wbroot = ts.getWorkbook()
     # updated = workbook.getWorksheet("D_UpdateTime").data['max_update_date-alias'][0]
     # updated = pd.to_datetime(updated, dayfirst=False)
