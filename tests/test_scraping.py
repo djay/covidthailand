69008c36028bddc647fcb204e53f40fd17ed7514
Kinshuk Dua <kinshukduaexam@gmail.com>
2021-10-05 16:51:45 +0530

    remove unused imports

--- tests/test_scraping.py
+++ tests/test_scraping.py
@@ -4,14 +4,13 @@ from utils_thai import file2date
 
 from bs4 import BeautifulSoup
 from utils_scraping import parse_file, pptx2chartdata, sanitize_filename
-from covid_data import briefing_case_types, briefing_deaths, briefing_deaths_provinces, briefing_deaths_summary, \
-                       briefing_documents, get_tests_by_area_chart_pptx, get_thai_situation_files, situation_pui_th, \
+from covid_data import briefing_case_types, briefing_deaths_provinces, briefing_deaths_summary, briefing_documents, \
+                       get_tests_by_area_chart_pptx, get_thai_situation_files, situation_pui_th, \
                        get_test_dav_files, vac_briefing_totals, vac_manuf_given, vac_slides_files, vaccination_daily, \
                        vaccination_reports_files2, vaccination_tables, get_tests_by_area_pdf, get_english_situation_files, \
                        situation_pui_en
 import pandas as pd
 import pytest
-from utils_pandas import export, import_csv
 import dateutil
 
 
