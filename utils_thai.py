4a936fb2269b7c0a6db6d6b024b560829b39e30c
modiholodri <reinhold.lauer@gmail.com>
2021-10-10 06:58:20 +0700

    Add rounding to thaipop formatter #86

--- utils_thai.py
+++ utils_thai.py
@@ -212,14 +212,14 @@ def parse_gender(x):
 
 
 def thaipop(num: float, pos: int) -> str:
-    pp = num / 69630000 * 100
-    num = num / 1000000
+    pp = round(num / 69630000 * 100, 1)
+    num = round(num / 1000000, 1)
     return '0%' if num > -0.1 and num < 0.1 else f'{pp:0.1f}%\n{num:.1f}M'.replace(".0", "")
 
 
 def thaipop2(num: float, pos: int) -> str:
-    pp = num / 69630000 / 2 * 100
-    num = num / 1000000
+    pp = round(num / 69630000 / 2 * 100, 1)
+    num = round(num / 1000000, 1)
     return '0%' if num > -0.1 and num < 0.1 else f'{num:.1f}M\n{pp:.1f}%'.replace(".0", "")
 
 
