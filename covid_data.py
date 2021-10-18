f3e078a1e0ce3383c6dab6fc7d530a5202569d17
Dylan Jay <software@pretaweb.com>
2021-10-19 00:56:36 +0700

    handle 2021-05-12 not having enought briefing prov cases

--- covid_data.py
+++ covid_data.py
@@ -1433,7 +1433,7 @@ def briefing_province_cases(date, pages):
     df = pd.DataFrame(data, columns=["Date", "Province", "Cases"]).set_index(["Date", "Province"])
     assert date >= d(
         "2021-01-13") and not df.empty, f"Briefing on {date} failed to parse cases per province"
-    if date > d("2021-05-01") and date not in [d("2021-07-18")]:
+    if date > d("2021-05-12") and date not in [d("2021-07-18")]:
         # TODO: 2021-07-18 has only 76 prov. not sure why yet. maybe doubled up or mispelled names?
         assert len(df.groupby("Province").count()) in [77, 78], f"Not enough provinces briefing {date}"
     return df
