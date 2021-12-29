fb1d4313f56df244441a7a577d93ab1188de584d
Dylan Jay <software@pretaweb.com>
2021-12-29 18:10:18 +0700

    fix assertion for wrong domestic number in briefing

--- covid_data_briefing.py
+++ covid_data_briefing.py
@@ -171,7 +171,7 @@ def briefing_case_types(date, pages, url):
             numbers, rest = get_next_numbers(text, "รวม", until="รายผู้ที่เดิน")
             cases, walkins, proactive, *quarantine = numbers
             domestic = get_next_number(rest, "ในประเทศ", return_rest=False, until="ราย")
-            if domestic and date not in [d("2021-11-22"), d("2021-12-02")]:
+            if domestic and date not in [d("2021-11-22"), d("2021-12-02"), d("2021-12-29")]:
                 assert domestic <= cases
                 assert domestic == walkins + proactive
             quarantine = quarantine[0] if quarantine else 0
