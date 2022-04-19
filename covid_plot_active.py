713deba2acfd649a7ff8d9b175dc583321a90722
Dylan Jay <software@pretaweb.com>
2022-04-19 19:19:54 +0700

    fix occupancy

--- covid_plot_active.py
+++ covid_plot_active.py
@@ -143,7 +143,7 @@ def save_active_plots(df: pd.DataFrame) -> None:
         'Hospitalized Severe',
         'Hospitalized All Mild',
     ]
-    peaks = df[cols] / df.rolling(7).mean().max(axis=0) * 100
+    peaks = df[cols] / df[cols].rolling(7).mean().max(axis=0) * 100
     peaks["Hospitalized Occupancy Level 2-3 %"] = df["Hospitalized Occupancy Level 2-3 %"]
     legends = [
         'In Serious Condition on Ventilator',
@@ -153,7 +153,7 @@ def save_active_plots(df: pd.DataFrame) -> None:
     ]
     plot_area(df=peaks,
               title='Active Covid Cases by Condition as % of Peak - Thailand',
-              png_prefix='active_peak', cols_subset=cols,
+              png_prefix='active_peak', cols_subset=list(peaks.columns),
               legends=legends,
               ma_days=7,
               kind='line', stacked=False, percent_fig=False, clean_end=True,
