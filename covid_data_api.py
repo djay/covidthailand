e11ad73df7d3bca3e519a903a0b93547bde9bda0
Dylan Jay <software@pretaweb.com>
2023-02-11 14:15:59 +0700

    fix when api paged is inaccessible

--- covid_data_api.py
+++ covid_data_api.py
@@ -363,7 +363,7 @@ def get_case_details_api_weekly():
 
     # df3 = load_paged_json("https://covid19.ddc.moph.go.th/api/Deaths/round-3-line-list", ["year", "weeknum"], target_date, dir="inputs/json/weekly")
     # df1 = load_paged_json("https://covid19.ddc.moph.go.th/api/Cases/round-1to2-line-lists", ["year", "weeknum"], target_date, dir="inputs/json/weekly")
-    df = load_paged_json("https://covid19.ddc.moph.go.th/api/Cases/round-4-line-lists",
+    df = load_paged_json("https://covid19.ddc.mpeoph.go.th/api/Cases/round-4-line-lists",
                          ["year", "weeknum"], None, dir="inputs/json/weekly/cases", timeout=140)
     df['age'] = pd.to_numeric(df['age_number'], errors="coerce")
     df = df.rename(columns=dict(province="province_of_onset"))
@@ -449,7 +449,7 @@ def load_paged_json(url, index=["year", "weeknum"], target_index=None, dir="inpu
     file, content, _ = next(iter(web_files(url, dir=None, check=check, appending=False, timeout=timeout, threads=4)), None)
     pagedata = json.loads(content) if content is not None else {}
     if "data" not in pagedata:
-        return pd.DataFrame(pagedata)
+        return pd.DataFrame(pagedata) if cached is None else cached
     page = pagedata['data']
     assert page
     last_page = pagedata['meta']['last_page']
@@ -483,7 +483,7 @@ def load_paged_json(url, index=["year", "weeknum"], target_index=None, dir="inpu
     pages_got = 0
     is_first = False
     urls = [f"{url}?page={p}" for p in pages]
-    for file, content, _ in web_files(*urls, dir=None, check=check, appending=False, timeout=timeout, threads=6):
+    for file, content, _ in web_files(*urls, dir=None, check=check, appending=False, timeout=timeout, threads=1):
         if file is None:
             if backwards:
                 df = pd.Dataframe()  # Can't join it. have eto give up
