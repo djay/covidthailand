86c6634fc8a2deecd616f613361b1c6b9aa24b84
Dylan Jay <software@pretaweb.com>
2023-04-23 22:17:57 +0700

    fix weekly prov api

--- covid_data_api.py
+++ covid_data_api.py
@@ -638,7 +638,7 @@ def timeline_by_province_weekly():
         os.rename(f"{dir}/{prefix}", week_file(max_week))
 
     # Get fake api files
-    cases2023 = pd.concat([pd.read_csv(week_file(week))
+    cases2023 = pd.concat([pd.read_json(week_file(week))
                           for week in range(1, max_week) if os.path.exists(week_file(week))] + [cases2023])
 
     df = pd.concat([df, cases2023])
